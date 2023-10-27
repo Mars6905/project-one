cdfdbca7288a35adf0af2f56c643601da0530911
Mars6905 <ibrogimzanovislam@gmail.com>
2023-10-27 18:49:56 +0600

    init

---
+++ dificalt.py
@@ -0,0 +1,10 @@
+
+# ans = (float(input('Введите bashy: ')))
+# ans2 = (float(input('Введите prosent:')))
+# ans3 = (int(input('Введите kolichestvo dney')))
+
+# for i in range(1 , ans3+1):
+#     print(i, float(ans))
+#     ans = ans + ans * (ans2/100)
+
+
---
+++ python.py
@@ -0,0 +1,747 @@
+# a = 12
+# print (a)
+
+
+# f = '//'
+# user = str(input('напиши //'))
+# if user == f:
+#     print('прив')
+
+
+# a = 'привет'
+# print(a.lower())
+
+# txt = 'apl gjjghhfndb db cn mm s skxjc apl'
+# print (txt.count('apl'))
+
+# a = '*'
+# print (a)
+
+# f = '*'
+# print ()
+
+# d = """
+      
+#       *
+#      ***   
+#     *****           
+#    *******              
+#   *********                 
+#      |||            
+
+
+# """
+# print (d)
+
+
+
+
+# user = input(str('Введите своё имя'))
+# print (user.upper())
+
+
+
+
+
+
+
+# манипуляция с индексом 
+# python
+# nohtyp
+# list = '!python!'
+# print(list[::-1])
+
+# def func():
+#     txt = 'jjjkkkk'
+
+# print (int(True))
+# print (int(False))
+
+
+
+
+
+
+
+
+# aas = str(122223)
+# print(type(aas))
+
+# задание№1
+# df = (str('Я web разработчик'))
+# print(len(df))
+# print(df + '.')
+# print('web' in df)
+# print(df[16])
+
+# как перенести строку в интеджер 
+# name = 'John'
+# age = 22
+# age = str(age)
+# print(name + age)
+# print(type(age))
+
+
+# import random
+# from random import *2
+# print(uniform(5,10))
+# print(randint(5,12))
+
+
+# laa = [1 , 2 , 344 , 1.33]
+
+# print(choice(laa))
+
+# name = input('a')
+# Name2 = input('что')
+# print(name.count(Name2))
+# frases = [1111, 1, 23, 22]
+
+# def rd():
+#     chance = [1 , 1 , 1]
+#     op = (choice(chance))
+#     if op == 1:
+#         print(choice(frases))
+#     else:
+#         pass
+
+
+# rd()
+# import time
+# def ts0():
+#     time.sleep(0.5)
+
+# def even(a):
+#     if a%2==0:
+#         print(a, 'Четное')
+#         ts0()
+#     else:
+#         print(a, 'Не четное')
+#         ts0()
+
+
+
+# ss = int(input('Скажи первое число;'))
+# ll = int(input('Скажи второе число;'))
+# ls = int(1)
+# ls + ll
+# for i in range(ss,ll+1):
+#     even(i)
+
+# def even(a):
+#     if a%2==0:
+#         print(a, 'Четное')
+#     else:
+#         print(a, 'Не четное')
+
+# ss = int(input('Скажи первое число;'))
+
+# ll = int(input('Скажи второе число;'))
+# ll + 1
+# for i in range(ss,ll+1):
+#     even(i)
+# ans2 = str(input('''Могу сортировать четные хочешь?
+# 1 = четные
+# 2 = нечетные 
+# 3 = не надо
+# какие вывести?
+# '''))
+# if ans2 == '1':
+#     for i in range(ss,ll+1):
+#         if i%2==0:
+#             print(i)
+# elif ans2 == '2':
+#     for i in range(ss,ll+1):
+#         if i%2!=0:
+#             print(i)
+# else:
+#     pass
+
+
+# fddd = ( 4, 0,6,7,3,2 , True , 2.333 , 'looool')
+# print(fddd[ ::-1])
+# ppp = 'а фиг тебе'
+# ans3 = str(input('Могу сохранить несколько ваших данных, впишите например что угодно;'))
+# ans4 = str(input('ещё что нибудь?'))
+# ans5 = str(input('Дробное число , через точку пожалуйста'))
+# ans6 = str(input('Записку'))
+# ans7 = str(input('Цифру'))
+# ans8 = str(input('Последний'))
+# total1 = [ans3 , ans4 , ans5 , ans6 , ans7 , ans8]
+# print (total1.append(ppp))
+
+
+
+
+# List_of_q = ['И таааак угадай число ;','Готов ? ну чтож ,погнали угадай число','Хочешь испытать удачу ? тогда угадай число',
+# 'А теперь сможешь угадать число и ты победил ;','Ну чтож победи меня , и угадай число','А теперь угадывай;',]
+# list_of_f = ['Хахахаха не угадал , попробуй ещё раз','Эх а я думал ты сможешь, давай ещё','Победителем не рождаются , им становиться , попробцй ещё',
+# 'Сложно конечно когда ты не знаешь ответа , давай ещё','Пробуй , ошибайся , и побеждай','Лузер) ну ладно попробуй ещё',
+# 'А шансы тают на глазах , но ты пробуй , вдруг попадешь','Мдаааа ты не побеждаешь , но ты пробуй пробуй','Ещё попытка',]
+# Listoflose_B = ['Число больше чем это','Число выше','Больше','Число находиться выше по значению','Загадонное число больше','Число больше по велечине',]
+# Listoflose_S = ['Сравнительно меньше','Число ниже','Меньше','Не такое большое','Мало','Недотягивает','Загадонное число меньше','Не такое большое','Меньше','Ниже','Это высоко , нужно ниже',]
+# List_of_WIN = ['Поздравляю с заслуженной победой','Отличная игра, ты настоящий победитель','Победа это всегда здоров ,ты это доказал'
+# ,'Ты сделал это , поздравляю','Невероятная игра конечно','Ха угадал , поздравляю','Победа достается тебе','Кажется ты угадал , поздравляю','Ты победил )','Чемпион)))'
+# ,'Мастерство и талант что сказать))','Браво ,эх еслиб я умел хлопать','Великолепно , отличная игра','Победитель )',
+# 'Сквозь не мало попыток ты всё таки победил','победа прекрасна, и она твоя','Блестяще Шерлок холмс )','Это элементарно Ватсон!'
+# ,'Отличная дедукция','ТЫЫ ЕСТЬ ГРУУУУУУТ','ТЫ смоооог ТЫЫЫЫЫ СМООООООООГ','ну I`ll be back','ВУДА ЛУДА ДАБ ДАБ','Чо такой серььезный?))'
+# ,'Ты гений без ни )','Как сказал человек ``Невозможное вожможно``','Я бы сказал фразу из сериала крестный отец ноооооо не буду','This is fine , yeah'
+# ,'Ты будто послушал ту самую песню ``INTS NOT OVER UNTIL III WIIINNN']
+
+# import random
+# def questxt():
+#     question= (choice(List_of_q))
+#     print(question)
+
+# def losttxt():
+#     lost= (choice(list_of_f))
+#     print(lost)
+
+# def nom_B():
+#     nomberisbig= (choice(Listoflose_B))
+#     print(nomberisbig)
+
+# def nom_S():
+#     nomberissmall= (choice(Listoflose_S))
+#     print(nomberissmall)
+
+# def WINTXT():
+#     WINER= (choice(List_of_WIN))
+#     print(WINER)
+
+# attc = 'У тебя попыток:'
+# def gn():
+#     ansg1 = int(input('Напиши первое число;'))
+#     ansg2 = int(input('Напиши второе число;'))
+#     att = int(input('Сколько дать вам попыток?'))
+#     nomber = random.randint(ansg1, ansg2)
+#     att_count_int = 0
+#     while att_count_int < att:
+#         guess = int(input(questxt()))
+#         if guess == nomber:
+#             print('YOU WIN')
+#             print(WINTXT())
+#             break
+#         elif guess < nomber:
+#             print('Не правильно')
+#             print(nom_B())
+#             print(losttxt())
+#         elif guess > nomber:
+#             print('Не правильно')
+#             print(nom_S())
+#             print(losttxt())
+#         else:
+#             print('Это не число но попытка снята')
+#         att_count_int = int(att_count_int)
+#         att_count_int += 1
+#         att_count = str(att_count_int)
+#         result = attc + att_count
+#         att_count_int = int(att_count)
+#         print(result)
+#     if att_count_int == att:
+#         print('Проигрыш , попытки закончились , загаданное число:' , nomber )
+
+# gn()
+
+
+
+
+# password = str('JH')
+# ans = str('ОТВЕТ')
+# while password == ans:
+#     ans = input('Ваш пароль;')
+#     if password == ans:
+#         print('ВЫ ВОШЛИ')
+#         password = True
+#         break
+#     elif password != ans:
+#         print('Не правильно')
+#     else:
+#         print('НЕ ПРАВИЛЬНО ПОПРОБУЙТЕ СНОВА')
+
+# i = ('JHON')
+# while i == 'JHON':
+#     ans = input('Нашипи')
+#     if ans == i:
+#         print('ВЫ ВОШЛИ')
+#         break
+#     else:
+#         print("не правильно пробуйте ещё")
+
+
+# while True:
+#     ans2 = input('ПИШИ ПАРОЛЬ:')
+#     passs = 'JOHN'
+#     if ans2 == passs:
+#         print('ВОШЛИ В СОСТЕМУ')
+#         break
+#     else:
+#         print('НЕ ПРАВИЛЬНО')
+
+# for x in range(6):
+#   print(0)
+
+# dfj = 'hello world'
+# p = dfj.upper()
+# print(p)
+
+# s = (list(' ghb'))
+# print(s)
+
+# # Разница в защищенности Https более защищеннный 
+
+# for i in range(0 ,101):
+#   print(i)
+
+# # 
+# answer = int(input('Введите оценку'))
+# if answer >= 100:
+#     print('A')
+# elif answer >= 80:
+#     print('B')
+# elif answer >= 70:
+#     print('С')
+# elif answer >= 60:
+#     print('D')
+# elif answer <= 50:
+#     print('F')
+# else:
+#     print('Это не число')
+
+
+# a = int(input('Ввидите число'))
+# if a%2==0:
+#     print('Число четное')
+# else:
+    # print('Число не четное')
+
+
+
+# mars = {'kay' : 'sjdd' ,'ключ' : 'значение'}
+# mars [1] = 12
+
+
+
+
+
+
+# mars = {1: 2, 2: 4}#1
+
+
+# mars ['fff'] = 12#2
+
+
+
+# data = ['fhhf', 'jdjjdd']
+# t = tuple(data)
+
+# mars[t] = [['djjjddj' , 'ssfsffs' , 'alalal']]#3
+
+
+# print(mars['fff'])#4
+
+
+# del mars[1]#5
+
+
+# print(len(mars)) #6
+
+# pip install python-telegram-bot 
+
+
+
+# txt = 'salam'
+# save = 'Кандайсын'
+
+
+# txt,save = save,txt
+# print(txt)
+
+
+# add = input('Введи число')
+# if add.isdigit == True:
+#     print('Правильно')
+
+# logg = 'PSG'
+# pasw = '!!'
+
+# while True:
+#     userlog = input('Пиши логин')
+#     if userlog == logg:
+#         print('Логин правильный')
+#         break
+#     else:
+#         print('Такой логин не существует')
+
+
+# while True:
+#     userpasw = input('Пиши пароль')
+#     if userpasw == pasw:
+#         print('Вы вошли в систему')
+#         break
+#     else:
+#         print('Пароль не верный')
+
+
+
+# for i in range(1, 6):
+#     print(str(i) * i)
+#     print(' ')
+
+
+
+# for add in range(1 , 51):
+#     if add < 30:
+#         print(add)
+        
+# f = 30
+# for i in range(30 , 51):
+#     if i%2==0:
+#         print(i)
+# ans = int(input('Привет , какая у тебя оценка?'))
+# if 0 < ans < 4:
+#     print('Плохо')
+# elif 3 < ans < 5:
+#     print('Хорошо')
+
+
+
+
+# name = "HELLO WORD"
+# print(name.lower())
+
+
+
+# job = ['persik' , 122 , 4555 , 6654 , 23.4 , True , 'loop']
+
+# num = [7,333,2]
+# # num.reverse()
+# # print(num)
+# num.sort()
+# print(num)
+
+
+
+
+
+# take_it = ['Jeff','Bessos','Jessika','Karl']
+# take_it.append('Karl2')
+# print(take_it)
+
+
+
+# name = str(input('Как тебя зовут?'))
+# # name.__reversed__()
+# print(name.__reversed__())
+
+
+# set = {5 , 4 , 1, 0 }
+# set.add(6)
+# print(set)
+# set.remove(1)
+# print(set)
+
+# a = 50
+# b = 60
+# a,b = b,a
+# print(a)
+
+
+# import calendar
+
+# # Задаем год и месяц
+# year = 2023
+# month = 10
+
+# # Получаем календарь для указанного месяца и года
+# cal = calendar.month(year, month)
+
+# # Разбиваем календарь на две строки
+# cal_lines = cal.splitlines()
+
+# # Выводим календарь в две строки
+# for line in cal_lines:
+#     print(line)
+
+# import calendar
+
+# import calendar
+# # for m in range(1, 13):
+# #     print(calendar.month(2023, m))
+# month = 4
+# # month = 4
+# print(calendar.month(month))
+
+
+
+
+
+
+
+# print("""
+# секунды = sec
+# минуты  = min
+# час     = hour
+# """)
+
+# ans = str(input('Введите что во что?'))
+# if ans == 'sec':
+#     pass
+# else:
+#     pass
+
+
+
+# if ans == 'min':
+#     pass
+# else:
+#     pass
+
+
+
+
+# if ans == 'hour':
+#     pass
+# else:
+#     pass
+
+
+
+# for i in range(0 , 51):
+#     if i <= 20:
+#         print(i)
+#         if i >= 20:
+#             for n in range(30 , 51 , 10):
+#                 print(n)
+            
+    # else:
+    #     print( i + 10)
+    #     if i == 50:
+    #         break
+    
+
+
+
+# lis = [255, 5, 8, -4.5, 10]
+
+# print(lis(slice(2)))
+
+
+
+
+
+# li=[12,45,5,54,6778,78,1]
+# limax = max(li)
+# print(limax)
+
+# listmy = [1,2,3,4,5,6,7,8,888,0]
+# listmy.insert(1 , '333433221')
+# print(listmy)
+
+# l = 'ff d fff dds aas s'
+# l.remove(' ')
+# print(l)
+
+
+
+
+
+
+# l = 'ff d fff dds aas s'
+# l = l.replace(' ', '')  # Удаляем пробелы
+# print(l)
+
+# s = ('223')
+# print(s.isdigit())
+
+
+
+# a = 11
+# b = 'djfj'
+# print(f'{b} {a}')
+
+
+# age = (22)
+# name = ('ddsasdd')
+# hgj = ' '
+# print(str(age) +hgj + name)
+
+# import builtins
+# print(len(dir(builtins)))
+
+
+
+# while True:
+#     st = str(input('Ведите слово :'))
+#     num = int(input('Введите количество :'))
+#     if num > 11:
+#         print('Слишком большое число , могу ввывести только не больше 10 повторов')
+#     else:
+#         for i in range(num):
+#             print(st)
+    
+# ans = (int(input('Введите число;')))
+# ans2 = (int(input('Введите сколько раз повторить в строке;')))
+# ans3 = (str(input('Введите что повторить;')))
+# for i in range(ans):
+#     print(ans3* ans2)
+
+# ans = (int(input('Введите число;')))
+
+
+
+
+
+
+# ans2 = (int(input('Введите stroku;')))
+# for i in range(11):
+#     print(i , ans2)
+
+
+
+
+
+# ans2 = (int(input('Введите chislo;')))
+
+# for i in range( 1 ,ans2+1):
+#     ans = i**2
+#     print('Квадрат числа' , i , 'равен' , ans)
+
+
+# ans2 = (int(input('Введите chislo;')))
+# for i in range(1,ans2):
+#     print('*'*i)
+
+
+
+
+
+# ans = (float(input('Введите bashy: ')))
+# ans2 = (float(input('Введите prosent:')))
+# ans3 = (int(input('Введите kolichestvo dney')))
+
+# for i in range(1 , ans3+1):
+#     print(i, float(ans))
+#     ans = ans + ans * (ans2/100)
+
+
+
+# ans3 = (int(input('pervoe chislo')))
+# ans = int(input('Vtoroe chislo'))
+# ans2 = int(input('step'))
+
+# for i in range (ans3 , ans , -ans2):
+#     print(i)
+ 
+
+# ans = int(input('Vvedite chislo:'))
+# ans2 = int(input('Vvedite chislo:'))
+
+# for i in range(ans , ans2 , -2):
+#     print(i)
+
+
+
+# ans = int(input('Vvedite chislo:'))
+# ans2 = int(input('Vvedite chislo:'))
+
+# for i in range(ans , ans2):
+#     if i%17==0 or i%10==9 or i%3==0 and i%5==0:
+#         print(i)
+        
+
+
+
+
+
+
+# ans = int(input('Vvedite chislo:'))
+
+# for i in range(1 , 11):
+#     print(ans , ' * ' , i , ' = ' , i*ans)
+        
+        
+
+
+
+
+# ans = str(input('Vvedite slovo:'))
+# while True:
+#     ans = str(input('Vvedite slovo:'))
+#     if ans=='konec':
+#         break
+#     else:
+#         print(ans)
+
+# answ = ['skkd','kkdks','s;a;lslls','wooeiiirutuyy','konec','ss;;dla']
+# for i in answ:
+#     if i != 'konec':
+#         print(i)
+#     elif i == 'kpnec':
+#         break
+
+
+
+
+# while True:
+#     ans = int(input('Vvedite chislo kotoroe delitsya na 5 :     '))
+#     if ans%5==0:
+#         print('Pravilno')
+#         break
+
+
+# passw = 1022
+# username = 'Kutya'
+# while True:
+#     answer = str(input('write name :  '))
+#     if answer == username:
+#         print('You right')
+#     answer2 = int(input('Write password :  '))
+#     if answer2 == passw:
+#         print('You right, it,s done , you may go in sistame')
+#         break
+#     if answer != username and answer2 !=passw :
+#         print('IT,S WRONG!! please try agan')
+
+
+
+
+
+
+# for i in range(1,31):
+#     if i == 25:
+#         continue
+#     else:
+#         print(i)
+
+
+
+
+
+
+
+# i = 0
+# while i < 30:
+#     i+=1
+#     if i !=25:
+#         print(i)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
